40bc291aa8fe9d90367ec6ac27c556445e5690d6
Harshitha2110 <68903353+Harshitha2110@users.noreply.github.com>
2020-10-26 19:19:12 +0530

    Create Armstrong.c

---
+++ Armstrong.c
@@ -0,0 +1,22 @@
+#include<studio.h>
+void main()
+{
+int n;
+int r=0,temp;
+printf("enter a no.");
+scanf("%d",&n);
+temp=n;
+while(n!=0)
+{
+temp=temp/10;
+r=r+r^3;
+temp/=10;
+}
+if(n==r)
+{
+printf("Armstrong");
+}
+else
+{
+printf("Not an Armstrong");
+}}
